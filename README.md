d6403914d9aa4f07921c4ba9befea5d95f9f06cb
Fabian Enos <fabian.enos@gmail.com>
2016-03-27 17:37:36 +0530

    Added tests through mocha and chai.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-node-fpgrow
-===========
+# node-fpgrowth
+
+### Testing
+
+You need to install mocha and chai to run tests
+
+```sh
+$  mocha test/fp-growth.js 
+```
+## License
+
+Code released under [the MIT license](https://github.com/fabianTMC/node-fpgrowth/blob/master/LICENSE).
--- test/fp-growth.js
+++ test/fp-growth.js
@@ -1,14 +1,82 @@
+// Include the testing library
+var chai = require('chai');
+var should = chai.should();
+
+// Include our module
 var fpgrowth = require('../lib/fp-growth');
 
-var baskets = [
-["e","a","d","b"],
-["d","a","c","e","b"],
-["c","a","b","e"],
-["b","a","d"],
-["d"],
-["d","b"],
-["a","d","e"],
-["b","c"]
-];
+// Support Percent related tests
+describe('SupportPercentTests', function() {
+  it('should throw an error because no support percentage was provided', function() {
+    (function() {
+        new fpgrowth();
+    }).should.throw("Pass a support percent");
+  });
 
-console.log(new fpgrowth(5).fromFile("./baskets.json").main());
+  it('should not throw an error because the support percentage was provided', function() {
+    (function() {
+        new fpgrowth(5);
+    }).should.not.throw("Pass a support percent");
+  });
+});
+
+// Baskets related tests
+describe('BasketsTests', function() {
+  it('should throw an error because no baskets were provided', function() {
+    (function() {
+        new fpgrowth(5).main();
+    }).should.throw("Baskets is not an array");
+  });
+
+    describe('FileTests', function() {
+          it('should throw an error because the filename was not provided', function() {
+            (function() {
+                new fpgrowth(5).fromFile().main();
+            }).should.throw("Pass a filename");
+          });
+
+          it('should throw an error because the file does not exist', function() {
+            (function() {
+                new fpgrowth(5).fromFile("./basketss.json").main();
+            }).should.throw("File does not exist");
+          });
+
+          it('should throw an error because the file contains invalid json', function() {
+            (function() {
+                new fpgrowth(5).fromFile("./test/invalid.json").main();
+            }).should.throw("Unexpected end of input");
+          });
+
+          it('should not throw an error because the file contains valid json', function() {
+            (function() {
+                new fpgrowth(5).fromFile("./test/baskets.json").main();
+            }).should.not.throw();
+          });
+    });
+
+    describe('ArrayTests', function() {
+           it('should throw an error because the baskets array is not provided', function() {
+            (function() {
+                new fpgrowth(5).fromArray().main();
+            }).should.throw("Pass the json array");
+          });
+
+          it('should throw an error because the baskets array is an object', function() {
+            (function() {
+                new fpgrowth(5).fromArray({}).main();
+            }).should.throw("Baskets is not an array");
+          });
+
+          it('should not throw an error because the baskets is an empty array', function() {
+            (function() {
+                new fpgrowth(5).fromArray([]).main();
+            }).should.not.throw("Baskets is not an array");
+          });
+
+          it('should not throw an error because the baskets array is an array of objects', function() {
+            (function() {
+                console.log(new fpgrowth(5).fromArray([{1: 1}, {2: 2}]).main());
+            }).should.not.throw("Baskets is not an array");
+          });
+    });
+});
